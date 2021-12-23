085c378e71119dbd44578f994630fb2890179708
ASUS\Fraza <adityafraja12@gmail.com>
2021-12-23 14:32:40 +0700

    add css reset dan card project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react"
+import '../assets/styling/cssReset.scss';
 import { Link } from "gatsby"
-import { Container, Grid, Typography, Card, Chip, CardContent, CardMedia, LinearProgress } from '@mui/material';
+import { Container, Grid, Typography, Card, Chip, CardContent, CardMedia, LinearProgress, Button } from '@mui/material';
 import Layout from "../components/layout/layout"
 import Seo from "../components/seo/seo"
 import './index.scss';
@@ -120,62 +121,80 @@ const IndexPage = () => (
 
         <Grid container>
           <Grid item>
-            <Card>
+            <Card className="card-project">
               <CardMedia
                 component="img"
-                height="150"
+                height="194"
                 image="/images/1.webp"
                 alt="Kapan Tongkang"
               />
               <CardContent>
-                <div>PMBB</div>
-                <h2>Padang Merdeka - PT Merdeka Bisnis Bersama</h2>
-                <Chip label="Restaurant" color="success" />
+                <Grid container sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                  <Grid item xs={9}>
+                  </Grid>
+                  <Grid item xs={3} className='btn-container-buy'>
+                    <Button variant="contained" className="btn-buy">
+                      <span className='f-buy'>BELI</span>
+                    </Button>
+                  </Grid>
+                </Grid>
+
+                <div className="card-code">PMBB</div>
+                <h2 className="card-title">Padang Merdeka - PT Merdeka Bisnis Bersama</h2>
+                <Chip label="Restaurant" color="success" variant="outlined" className="chip-text" />
 
                 <Grid container sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                  <Grid item xs={6}>
-                    <span>Rp 3.750.000.000 dari Rp 3.750.000.000 Total Pendanaan</span>
+                  <Grid item xs={9} className='card-price'>
+                    Rp 3.750.000.000
                   </Grid>
-                  <Grid item xs={6}>
-                    <span>0 Hari Lagi</span>
+                  <Grid item xs={3} className='card-price'>
+                    0
+                  </Grid>
+                </Grid>
+                <Grid container>
+                  <Grid item xs={9} className='card-text-detail'>
+                    dari Rp 3.750.000.000 Total Pendanaan
+                  </Grid>
+                  <Grid item xs={3} className='card-text-detail'>
+                    Hari Lagi
                   </Grid>
                 </Grid>
 
                 <LinearProgress variant="determinate" value={50} />
 
                 <Grid container sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                  <Grid item xs={6}>
+                  <Grid item xs={6} className='card-text-detail'>
                     <span>Harga Per Lot</span>
                   </Grid>
-                  <Grid item xs={6}>
+                  <Grid item xs={6} className='card-text-detail'>
                     <span>Jumlah Lot</span>
                   </Grid>
                 </Grid>
 
                 <Grid container sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                  <Grid item xs={6}>
+                  <Grid item xs={6} className='card-title'>
                     <span>Rp. 1.000.000</span>
                   </Grid>
-                  <Grid item xs={6}>
+                  <Grid item xs={6} className='card-title'>
                     <span>3750</span>
                   </Grid>
                 </Grid>
 
                 <Grid container sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                  <Grid item xs={6}>
+                  <Grid item xs={6} className='card-text-detail'>
                     <span>Periode Dividen</span>
                   </Grid>
-                  <Grid item xs={6}>
+                  <Grid item xs={6} className='card-text-detail'>
                     <span>Estimasi Dividen</span>
                   </Grid>
                 </Grid>
 
                 <Grid container sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                  <Grid item xs={6}>
+                  <Grid item xs={6} className='card-title'>
                     <span>Per 3 bulan</span>
                   </Grid>
                   <Grid item xs={6}>
-                    <span>10-20% Per Tahun</span>
+                    <span className='card-title'>10-20%</span> <span className='card-text-detail-2'>Per Tahun</span>
                   </Grid>
                 </Grid>
               </CardContent>
